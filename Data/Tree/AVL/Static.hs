c3b0b1bc956d5639656af07bc707678909687b60
Federico Lebrón <federico.lebron@gmail.com>
2014-01-04 02:01:46 +0000

    Adding size function, stored in an AVLTree.

--- Data/Tree/AVL/Static.hs
+++ Data/Tree/AVL/Static.hs
@@ -3,6 +3,7 @@
 module Data.Tree.AVL.Static (
   AVLTree,
   empty,
+  size,
   delete,
   value,
   insert,
@@ -15,17 +16,20 @@ module Data.Tree.AVL.Static (
 import Data.Tree.AVL.Static.Internal
 
 empty :: AVLTree a
-empty = T Nil
+empty = T Nil 0
+
+size :: AVLTree a -> Integer
+size (T _ k) = k
 
 insert :: Ord a => a -> AVLTree a -> AVLTree a
-insert x t@(T root) = case zipTo x (unZip root) of
+insert x t = case zipTo x (unZip t) of
   Zipper Nil ctx -> insertUnbalancedAt (Balanced x Nil Nil) ctx
   _ -> t
 
 search :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
-search x (T root) = case zipTo x (unZip root) of
+search x t = case zipTo x (unZip t) of
   Zipper Nil _ -> Nothing
-  t -> Just t
+  z -> Just z
 
 predecessor :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
 predecessor x t = search x t >>= zipToPredecessor
@@ -34,6 +38,6 @@ successor :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
 successor x t = search x t >>= zipToSuccessor
 
 delete :: Ord a => a -> AVLTree a -> AVLTree a
-delete x t@(T root) = case zipTo x $ unZip root of
+delete x t = case zipTo x (unZip t) of
   Zipper Nil _ -> t
   z -> deleteBST z
--- Data/Tree/AVL/Static/Internal.hs
+++ Data/Tree/AVL/Static/Internal.hs
@@ -37,7 +37,7 @@ data AVLNode :: Nat -> * -> * where
 
 deriving instance Show a => Show (AVLNode n a)
 
-data AVLTree a = forall n. T (AVLNode n a)
+data AVLTree a = forall n. T (AVLNode n a) Integer
 deriving instance Show a => Show (AVLTree a)
 
 foldNode :: (b -> b -> a -> b) -> b -> AVLNode n a -> b
@@ -65,13 +65,13 @@ traverseNode f (Leftie x l r) = flip Leftie <$> traverseNode f l
                                             <*> f x
                                             <*> traverseNode f r
 instance Functor AVLTree where
-  fmap f (T k) = T (fmapNode f k)
+  fmap f (T r k) = T (fmapNode f r) k
 
 instance Foldable AVLTree where
-  foldMap f (T r) = foldNode (\x y z -> x <> z <> y) mempty (fmapNode f r)
+  foldMap f (T r _) = foldNode (\x y z -> x <> z <> y) mempty (fmapNode f r)
 
 instance Traversable AVLTree where
-  traverse f (T r) = T <$> traverseNode f r
+  traverse f (T r k) = flip T k <$> traverseNode f r
 
 {- Context for a Zipper.
  - BC = Balanced Context,
@@ -85,7 +85,7 @@ data Context :: Nat -> * -> * where
   LLC :: a -> AVLNode n a -> Context (Succ (Succ n)) a -> Context (Succ n) a
   RLC :: a -> AVLNode (Succ n) a -> Context (Succ (Succ n)) a -> Context n a
   RRC :: a -> AVLNode n a -> Context (Succ (Succ n)) a -> Context (Succ n) a
-  Root :: Context n a
+  Root :: Integer -> Context n a
 deriving instance Show a => Show (Context n a)
 
 data Zipper a = forall n. Zipper (AVLNode n a) (Context n a)
@@ -97,12 +97,12 @@ value (Zipper (Leftie x _ _) _) = x
 value (Zipper (Rightie x _ _) _) = x
 value (Zipper Nil _) = error "Zipper points to Nil."
 
-unZip :: AVLNode n a -> Zipper a
-unZip = flip Zipper Root
+unZip :: AVLTree a -> Zipper a
+unZip (T r k) = Zipper r (Root k)
 
-zipUp :: Zipper a -> AVLTree a
-zipUp (Zipper t Root) = T t
-zipUp z = zipUp $ up z
+zipUp :: (Integer -> Integer) -> Zipper a -> AVLTree a
+zipUp f (Zipper t (Root k)) = T t (f k)
+zipUp f z = zipUp f (up z)
 
 up :: Zipper a -> Zipper a
 up (Zipper x (LRC v y ctx)) = Zipper (Leftie v y x) ctx
@@ -112,10 +112,10 @@ up (Zipper x (RRC v y ctx)) = Zipper (Rightie v y x) ctx
 up (Zipper x (BC True v y ctx)) = Zipper (Balanced v x y) ctx
 up (Zipper x (BC False v y ctx)) = Zipper (Balanced v y x) ctx
 -- TODO: Should I error out on Root?
-up (Zipper x Root) = Zipper x Root
+up z@(Zipper _ (Root _)) = z
 
 canGoUp :: Zipper a -> Bool
-canGoUp (Zipper _ Root) = False
+canGoUp (Zipper _ (Root _)) = False
 canGoUp _ = True
 
 left :: Zipper a -> Zipper a
@@ -166,15 +166,15 @@ zipTo x z = let v = value z
                  GT -> zipTo x $ right z
 
 insertUnbalancedAt :: AVLNode (Succ n) a -> Context n a -> AVLTree a
-insertUnbalancedAt t (LRC v y ctx) = zipUp $ Zipper (Balanced v y t) ctx
-insertUnbalancedAt t (RLC v y ctx) = zipUp $ Zipper (Balanced v t y) ctx
-insertUnbalancedAt t Root = T t
+insertUnbalancedAt t (LRC v y ctx) = zipUp (+1) $ Zipper (Balanced v y t) ctx
+insertUnbalancedAt t (RLC v y ctx) = zipUp (+1) $ Zipper (Balanced v t y) ctx
+insertUnbalancedAt t (Root k) = T t (k + 1)
 
 {- LLC -}
-insertUnbalancedAt (Leftie b g p) (LLC a d ctx) = zipUp z
+insertUnbalancedAt (Leftie b g p) (LLC a d ctx) = zipUp (+1) z
   where
     z = Zipper (Balanced b g (Balanced a p d)) ctx
-insertUnbalancedAt (Rightie b g q) (LLC a d ctx) = zipUp z
+insertUnbalancedAt (Rightie b g q) (LLC a d ctx) = zipUp (+1) z
   where
     z = Zipper t ctx
     t = case q of
@@ -186,10 +186,10 @@ insertUnbalancedAt (Balanced b g p) (LLC a d ctx) = goUp
     goUp = insertUnbalancedAt (Rightie b g (Leftie a p d)) ctx
 
 {- RRC -}
-insertUnbalancedAt (Rightie b g p) (RRC a d ctx) = zipUp z
+insertUnbalancedAt (Rightie b g p) (RRC a d ctx) = zipUp (+1) z
   where
     z = Zipper (Balanced b (Balanced a d g) p) ctx
-insertUnbalancedAt (Leftie b q p) (RRC a d ctx) = zipUp z
+insertUnbalancedAt (Leftie b q p) (RRC a d ctx) = zipUp (+1) z
   where
     z = Zipper t ctx
     t = case q of
@@ -215,7 +215,7 @@ zipToSuccessor z | canGoRight z = Just . zipToSmallest $ right z
                  | otherwise = let parent = zipToFirstLeftChild z
                                in  up <$> parent
 zipToPredecessor :: Zipper a -> Maybe (Zipper a)
-zipToPredecessor z | canGoLeft z = Just . zipToSmallest $ left z
+zipToPredecessor z | canGoLeft z = Just . zipToSmallest $ left z
                    | otherwise = let parent = zipToFirstRightChild z
                                  in up <$> parent
 
@@ -224,12 +224,12 @@ zipToSmallest z | canGoLeft z = zipToSmallest (left z)
                 | otherwise = z
 
 zipToGreatest :: Zipper a -> Zipper a
-zipToGreatest z | canGoRight z = zipToGreatest (right z)
+zipToGreatest z | canGoRight z = zipToGreatest (right z)
                 | otherwise = z
 
 zipToFirstLeftChild :: Zipper a -> Maybe (Zipper a)
 zipToFirstLeftChild z | isLeft z = Just z
-zipToFirstLeftChild z | canGoUp z = zipToFirstLeftChild (up z)
+zipToFirstLeftChild z | canGoUp z = zipToFirstLeftChild (up z)
                       | otherwise = Nothing
 
 zipToFirstRightChild :: Zipper a -> Maybe (Zipper a)
@@ -242,7 +242,7 @@ fixContext k k' = go
   where
     z x = if x == k then k' else x
     go :: Context n' a -> Context n' a
-    go Root = Root
+    go r@(Root _) = r
     go (BC goLeft x y ctx) = BC goLeft (z x) y (go ctx)
     go (LLC x y ctx) = LLC (z x) y (go ctx)
     go (LRC x y ctx) = LRC (z x) y (go ctx)
@@ -274,12 +274,12 @@ deleteBST z@(Zipper (Balanced k _ _) _) =
 deleteBST (Zipper Nil _) = error "You cannot delete Nil."
 
 rebalance :: forall a n. AVLNode n a -> Context (Succ n) a -> AVLTree a
-rebalance t Root = T t
-rebalance t (BC True a d ctx) = zipUp $ Zipper (Rightie a t d) ctx
-rebalance t (BC False a d ctx) = zipUp $ Zipper (Leftie a d t) ctx
+rebalance t (Root k) = (T t (k - 1))
+rebalance t (BC True a d ctx) = zipUp (subtract 1) $ Zipper (Rightie a t d) ctx
+rebalance t (BC False a d ctx) = zipUp (subtract 1) $ Zipper (Leftie a d t) ctx
 rebalance t (LLC a d ctx) = rebalance (Balanced a t d) ctx
 rebalance t (RRC a d ctx) = rebalance (Balanced a d t) ctx
-rebalance t (RLC a (Balanced d t1 t2) ctx) = zipUp z
+rebalance t (RLC a (Balanced d t1 t2) ctx) = zipUp (subtract 1) z
   where
     z = Zipper (Leftie d (Rightie a t t1) t2) ctx
 rebalance t (RLC a (Rightie d t1 t2) ctx) = rebalance z ctx
@@ -291,7 +291,7 @@ rebalance t (RLC a (Leftie d q t2) ctx) = rebalance z ctx
           Leftie t1 p1 p2 -> Balanced t1 (Balanced a t p1) (Rightie d p2 t2)
           Rightie t1 p1 p2 ->  Balanced t1 (Leftie a t p1) (Balanced d p2 t2)
           Balanced t1 p1 p2 -> Balanced t1 (Balanced a t p1) (Balanced d p2 t2)
-rebalance t (LRC a (Balanced d t1 t2) ctx) = zipUp $ z
+rebalance t (LRC a (Balanced d t1 t2) ctx) = zipUp (subtract 1) z
   where
     z = Zipper (Rightie d t1 (Leftie a t2 t)) ctx
 rebalance t (LRC a (Leftie d t1 t2) ctx) = rebalance z ctx
