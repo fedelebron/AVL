4591deed082a1d2eb9e94fe46afeec1b0834a709
Federico Lebrón <federico.lebron@gmail.com>
2014-01-04 01:29:00 +0000

    Removed type parameter m in Context and Zipper.
    m isn't needed as of this moment, so it's been removed. With it, go
    Iterator (which is now an honest Zipper) and the m parameter in Zipper,
    which now just takes the type of the elements in the tree it points to.

--- Data/Tree/AVL/Static.hs
+++ Data/Tree/AVL/Static.hs
@@ -4,19 +4,15 @@ module Data.Tree.AVL.Static (
   AVLTree,
   empty,
   delete,
-  get,
+  value,
   insert,
   search,
   predecessor,
   successor,
-  Iterator
+  Zipper
 ) where
 
 import Data.Tree.AVL.Static.Internal
-
-data Iterator a = forall m. Iterator (Zipper m a)
-get :: Iterator a -> a
-get (Iterator z) = value z
 
 empty :: AVLTree a
 empty = T Nil
@@ -26,22 +22,16 @@ insert x t@(T root) = case zipTo x (unZip root) of
   Zipper Nil ctx -> insertUnbalancedAt (Balanced x Nil Nil) ctx
   _ -> t
 
-search :: Ord a => a -> AVLTree a -> Maybe (Iterator a)
+search :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
 search x (T root) = case zipTo x (unZip root) of
   Zipper Nil _ -> Nothing
-  t -> Just (Iterator t)
+  t -> Just t
 
-predecessor :: Ord a => a -> AVLTree a -> Maybe (Iterator a)
-predecessor x t = do
-                    Iterator z <- search x t
-                    p <- zipToPredecessor z
-                    return $ Iterator p
+predecessor :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
+predecessor x t = search x t >>= zipToPredecessor
 
-successor :: Ord a => a -> AVLTree a -> Maybe (Iterator a)
-successor x t = do
-                  Iterator z <- search x t
-                  p <- zipToSuccessor z
-                  return $ Iterator p
+successor :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
+successor x t = search x t >>= zipToSuccessor
 
 delete :: Ord a => a -> AVLTree a -> AVLTree a
 delete x t@(T root) = case zipTo x $ unZip root of
--- Data/Tree/AVL/Static/Internal.hs
+++ Data/Tree/AVL/Static/Internal.hs
@@ -79,32 +79,32 @@ instance Traversable AVLTree where
  -       For instance, LRC means a Leftie context, where one has taken the Right
  -       path in the subtree. -}
 
-data Context :: Nat -> Nat -> * -> * where
-  BC :: Bool -> a -> AVLNode n a -> Context m (Succ n) a -> Context m n a
-  LRC :: a -> AVLNode (Succ n) a -> Context m (Succ (Succ n)) a -> Context m n a
-  LLC :: a -> AVLNode n a -> Context m (Succ (Succ n)) a -> Context m (Succ n) a
-  RLC :: a -> AVLNode (Succ n) a -> Context m (Succ (Succ n)) a -> Context m n a
-  RRC :: a -> AVLNode n a -> Context m (Succ (Succ n)) a -> Context m (Succ n) a
-  Root :: Context n n a
-deriving instance Show a => Show (Context m n a)
+data Context :: Nat -> * -> * where
+  BC :: Bool -> a -> AVLNode n a -> Context (Succ n) a -> Context n a
+  LRC :: a -> AVLNode (Succ n) a -> Context (Succ (Succ n)) a -> Context n a
+  LLC :: a -> AVLNode n a -> Context (Succ (Succ n)) a -> Context (Succ n) a
+  RLC :: a -> AVLNode (Succ n) a -> Context (Succ (Succ n)) a -> Context n a
+  RRC :: a -> AVLNode n a -> Context (Succ (Succ n)) a -> Context (Succ n) a
+  Root :: Context n a
+deriving instance Show a => Show (Context n a)
 
-data Zipper m a = forall n. Zipper (AVLNode n a) (Context m n a)
-deriving instance Show a => Show (Zipper m a)
+data Zipper a = forall n. Zipper (AVLNode n a) (Context n a)
+deriving instance Show a => Show (Zipper a)
 
-value :: Zipper m a -> a
+value :: Zipper a -> a
 value (Zipper (Balanced x _ _) _) = x
 value (Zipper (Leftie x _ _) _) = x
 value (Zipper (Rightie x _ _) _) = x
 value (Zipper Nil _) = error "Zipper points to Nil."
 
-unZip :: AVLNode m a -> Zipper m a
+unZip :: AVLNode n a -> Zipper a
 unZip = flip Zipper Root
 
-zipUp :: Zipper m a -> AVLNode m a
-zipUp (Zipper t Root) = t
+zipUp :: Zipper a -> AVLTree a
+zipUp (Zipper t Root) = T t
 zipUp z = zipUp $ up z
 
-up :: Zipper m a -> Zipper m a
+up :: Zipper a -> Zipper a
 up (Zipper x (LRC v y ctx)) = Zipper (Leftie v y x) ctx
 up (Zipper x (LLC v y ctx)) = Zipper (Leftie v x y) ctx
 up (Zipper x (RLC v y ctx)) = Zipper (Rightie v x y) ctx
@@ -114,50 +114,50 @@ up (Zipper x (BC False v y ctx)) = Zipper (Balanced v y x) ctx
 -- TODO: Should I error out on Root?
 up (Zipper x Root) = Zipper x Root
 
-canGoUp :: Zipper m a -> Bool
+canGoUp :: Zipper a -> Bool
 canGoUp (Zipper _ Root) = False
 canGoUp _ = True
 
-left :: Zipper m a -> Zipper m a
+left :: Zipper a -> Zipper a
 left (Zipper (Balanced x l r) ctx) = Zipper l (BC True x r ctx)
 left (Zipper (Leftie x l r) ctx) = Zipper l (LLC x r ctx)
 left (Zipper (Rightie x l r) ctx) = Zipper l (RLC x r ctx)
 -- TODO: Should I error out on Nil?
 left z = z
 
-right :: Zipper m a -> Zipper m a
+right :: Zipper a -> Zipper a
 right (Zipper (Rightie x l r) ctx) = Zipper r (RRC x l ctx)
 right (Zipper (Leftie x l r) ctx) = Zipper r (LRC x l ctx)
 right (Zipper (Balanced x l r) ctx) = Zipper r (BC False x l ctx)
 -- TODO: Should I error out on Nil?
 right z = z
 
-canGoLeft :: Zipper m a -> Bool
+canGoLeft :: Zipper a -> Bool
 canGoLeft (Zipper (Rightie _ Nil _) _) = False
 canGoLeft (Zipper (Balanced _ Nil _) _) = False
 canGoLeft (Zipper Nil _) = False
 canGoLeft _ = True
 
-canGoRight :: Zipper m a -> Bool
+canGoRight :: Zipper a -> Bool
 canGoRight (Zipper (Leftie _ _ Nil) _) = False
 canGoRight (Zipper (Balanced _ _ Nil) _ ) = False
 canGoRight (Zipper Nil _) = False
 canGoRight _ = True
 
-isLeft :: Zipper m a -> Bool
+isLeft :: Zipper a -> Bool
 isLeft z | not (canGoUp z) = False
 isLeft (Zipper _ (BC False _ _ _)) = False
 isLeft (Zipper _ (RRC _ _ _)) = False
 isLeft (Zipper _ (LRC _ _ _ )) = False
 isLeft _ = True
 
-isRight :: Zipper m a -> Bool
+isRight :: Zipper a -> Bool
 isRight = (&&) <$> canGoUp <*> (not . isLeft)
 
-isLeaf :: Zipper m a -> Bool
+isLeaf :: Zipper a -> Bool
 isLeaf = (&&) <$> (not . canGoLeft) <*> (not . canGoRight)
 
-zipTo :: Ord a => a -> Zipper m a -> Zipper m a
+zipTo :: Ord a => a -> Zipper a -> Zipper a
 zipTo _ z@(Zipper Nil _) = z
 zipTo x z = let v = value z
             in case compare x v of
@@ -165,16 +165,16 @@ zipTo x z = let v = value z
                  LT -> zipTo x $ left z
                  GT -> zipTo x $ right z
 
-insertUnbalancedAt :: AVLNode (Succ n) a -> Context m n a -> AVLTree a
-insertUnbalancedAt t (LRC v y ctx) = T . zipUp $ Zipper (Balanced v y t) ctx
-insertUnbalancedAt t (RLC v y ctx) = T . zipUp $ Zipper (Balanced v t y) ctx
+insertUnbalancedAt :: AVLNode (Succ n) a -> Context n a -> AVLTree a
+insertUnbalancedAt t (LRC v y ctx) = zipUp $ Zipper (Balanced v y t) ctx
+insertUnbalancedAt t (RLC v y ctx) = zipUp $ Zipper (Balanced v t y) ctx
 insertUnbalancedAt t Root = T t
 
 {- LLC -}
-insertUnbalancedAt (Leftie b g p) (LLC a d ctx) = T $ zipUp z
+insertUnbalancedAt (Leftie b g p) (LLC a d ctx) = zipUp z
   where
     z = Zipper (Balanced b g (Balanced a p d)) ctx
-insertUnbalancedAt (Rightie b g q) (LLC a d ctx) = T $ zipUp z
+insertUnbalancedAt (Rightie b g q) (LLC a d ctx) = zipUp z
   where
     z = Zipper t ctx
     t = case q of
@@ -186,10 +186,10 @@ insertUnbalancedAt (Balanced b g p) (LLC a d ctx) = goUp
     goUp = insertUnbalancedAt (Rightie b g (Leftie a p d)) ctx
 
 {- RRC -}
-insertUnbalancedAt (Rightie b g p) (RRC a d ctx) = T $ zipUp z
+insertUnbalancedAt (Rightie b g p) (RRC a d ctx) = zipUp z
   where
     z = Zipper (Balanced b (Balanced a d g) p) ctx
-insertUnbalancedAt (Leftie b q p) (RRC a d ctx) = T $ zipUp z
+insertUnbalancedAt (Leftie b q p) (RRC a d ctx) = zipUp z
   where
     z = Zipper t ctx
     t = case q of
@@ -210,38 +210,38 @@ insertUnbalancedAt (Rightie b g p) (BC False a d ctx) = goUp
 insertUnbalancedAt t (BC False a d ctx) = insertUnbalancedAt (Rightie a d t) ctx
 insertUnbalancedAt t (BC True a d ctx) = insertUnbalancedAt (Leftie a t d) ctx
 
-zipToSuccessor :: Zipper m a -> Maybe (Zipper m a)
+zipToSuccessor :: Zipper a -> Maybe (Zipper a)
 zipToSuccessor z | canGoRight z = Just . zipToSmallest $ right z
                  | otherwise = let parent = zipToFirstLeftChild z
                                in  up <$> parent
-zipToPredecessor :: Zipper m a -> Maybe (Zipper m a)
+zipToPredecessor :: Zipper a -> Maybe (Zipper a)
 zipToPredecessor z | canGoLeft z = Just . zipToSmallest $ left z
                    | otherwise = let parent = zipToFirstRightChild z
                                  in up <$> parent
 
-zipToSmallest :: Zipper m a -> Zipper m a
+zipToSmallest :: Zipper a -> Zipper a
 zipToSmallest z | canGoLeft z = zipToSmallest (left z)
                 | otherwise = z
 
-zipToGreatest :: Zipper m a -> Zipper m a
+zipToGreatest :: Zipper a -> Zipper a
 zipToGreatest z | canGoRight z = zipToGreatest (right z)
                 | otherwise = z
 
-zipToFirstLeftChild :: Zipper m a -> Maybe (Zipper m a)
+zipToFirstLeftChild :: Zipper a -> Maybe (Zipper a)
 zipToFirstLeftChild z | isLeft z = Just z
 zipToFirstLeftChild z | canGoUp z = zipToFirstLeftChild (up z)
                       | otherwise = Nothing
 
-zipToFirstRightChild :: Zipper m a -> Maybe (Zipper m a)
+zipToFirstRightChild :: Zipper a -> Maybe (Zipper a)
 zipToFirstRightChild z | isRight z = Just z
 zipToFirstRightChild z | canGoUp z = zipToFirstRightChild (up z)
                        | otherwise = Nothing
 
-fixContext :: forall m a n. Eq a => a -> a -> Context m n a -> Context m n a
+fixContext :: forall a n. Eq a => a -> a -> Context n a -> Context n a
 fixContext k k' = go
   where
     z x = if x == k then k' else x
-    go :: Context m' n' a -> Context m' n' a
+    go :: Context n' a -> Context n' a
     go Root = Root
     go (BC goLeft x y ctx) = BC goLeft (z x) y (go ctx)
     go (LLC x y ctx) = LLC (z x) y (go ctx)
@@ -249,7 +249,7 @@ fixContext k k' = go
     go (RRC x y ctx) = RRC (z x) y (go ctx)
     go (RLC x y ctx) = RLC (z x) y (go ctx)
 
-deleteBST :: Eq a => Zipper m a -> AVLTree a
+deleteBST :: Eq a => Zipper a -> AVLTree a
 deleteBST (Zipper (Balanced _ Nil Nil) ctx) = rebalance Nil ctx
 deleteBST (Zipper (Rightie _ Nil r) ctx) = rebalance r ctx
 deleteBST (Zipper (Leftie _ l Nil) ctx) = rebalance l ctx
@@ -273,13 +273,13 @@ deleteBST z@(Zipper (Balanced k _ _) _) =
        _ -> error "The impossible has happened, bad successor found."
 deleteBST (Zipper Nil _) = error "You cannot delete Nil."
 
-rebalance :: forall m a n. AVLNode n a -> Context m (Succ n) a -> AVLTree a
+rebalance :: forall a n. AVLNode n a -> Context (Succ n) a -> AVLTree a
 rebalance t Root = T t
-rebalance t (BC True a d ctx) = T . zipUp $ Zipper (Rightie a t d) ctx
-rebalance t (BC False a d ctx) = T . zipUp $ Zipper (Leftie a d t) ctx
+rebalance t (BC True a d ctx) = zipUp $ Zipper (Rightie a t d) ctx
+rebalance t (BC False a d ctx) = zipUp $ Zipper (Leftie a d t) ctx
 rebalance t (LLC a d ctx) = rebalance (Balanced a t d) ctx
 rebalance t (RRC a d ctx) = rebalance (Balanced a d t) ctx
-rebalance t (RLC a (Balanced d t1 t2) ctx) = T . zipUp $ z
+rebalance t (RLC a (Balanced d t1 t2) ctx) = zipUp z
   where
     z = Zipper (Leftie d (Rightie a t t1) t2) ctx
 rebalance t (RLC a (Rightie d t1 t2) ctx) = rebalance z ctx
@@ -291,7 +291,7 @@ rebalance t (RLC a (Leftie d q t2) ctx) = rebalance z ctx
           Leftie t1 p1 p2 -> Balanced t1 (Balanced a t p1) (Rightie d p2 t2)
           Rightie t1 p1 p2 ->  Balanced t1 (Leftie a t p1) (Balanced d p2 t2)
           Balanced t1 p1 p2 -> Balanced t1 (Balanced a t p1) (Balanced d p2 t2)
-rebalance t (LRC a (Balanced d t1 t2) ctx) = T . zipUp $ z
+rebalance t (LRC a (Balanced d t1 t2) ctx) = zipUp $ z
   where
     z = Zipper (Rightie d t1 (Leftie a t2 t)) ctx
 rebalance t (LRC a (Leftie d t1 t2) ctx) = rebalance z ctx
