fedelebron/AVL

commit 1895f88098c02e334877fbbff6cfc20cd9f2dc3f

Added Haddock documentation.

Data/Tree/AVL/Static/Internal.hs
@@ -6,19 +6,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Data.Tree.AVL.Static.Internal (
-  AVLNode(..),
-  AVLTree(T),
-  insertUnbalancedAt,
-  deleteBST,
-  unZip,
-  value,
-  zipTo,
-  zipToSmallest,
-  zipToPredecessor,
-  zipToSuccessor,
-  Zipper(Zipper)
-) where
+module Data.Tree.AVL.Static.Internal where
 
 import Prelude hiding (fmap)
 import Control.Applicative (Applicative, pure, (<$>), (<*>))
@@ -27,8 +15,11 @@ import Data.Traversable (Traversable, traverse)
 import Data.Monoid (Monoid, mempty, (<>))
 import Data.Foldable (Foldable, foldMap)
 
+-- |A natural number datatype, hoisted to a Kind using DataKinds.
 data Nat = Zero | Succ Nat deriving (Eq, Ord, Show)
 
+-- |An @'AVLNode' n a@ is a node whose subtree has height @n@, and values of
+-- type @a@.
 data AVLNode :: Nat -> * -> * where
   Nil :: AVLNode Zero a
   Leftie :: a -> AVLNode (Succ n) a -> AVLNode n a -> AVLNode (Succ (Succ n)) a
@@ -36,7 +27,8 @@ data AVLNode :: Nat -> * -> * where
   Balanced :: a -> AVLNode n a -> AVLNode n a -> AVLNode (Succ n) a
 
 deriving instance Show a => Show (AVLNode n a)
-
+-- |An @'AVLTree' a@ is a statically balanced tree, whose non-nil values
+-- have type @a@.
 data AVLTree a = forall n. T (AVLNode n a) Integer
 deriving instance Show a => Show (AVLTree a)
 
@@ -46,7 +38,6 @@ foldNode f e (Balanced x l r) = f (foldNode f e l) (foldNode f e r) x
 foldNode f e (Rightie x l r) = f (foldNode f e l) (foldNode f e r) x
 foldNode f e (Leftie x l r) = f (foldNode f e l) (foldNode f e r) x
 
-{- Note, this isn't actually a Functor, since we require f monotonic. -}
 fmapNode :: (a -> b) -> AVLNode n a -> AVLNode n b
 fmapNode _ Nil = Nil
 fmapNode f (Balanced x l r) = Balanced (f x) (fmapNode f l) (fmapNode f r)
@@ -65,45 +56,84 @@ traverseNode f (Leftie x l r) = flip Leftie <$> traverseNode f l
                                             <*> f x
                                             <*> traverseNode f r
 instance Functor AVLTree where
+  -- |Functor instance for AVLNodes. Note, this isn't actually a Functor, since
+  -- we require f monotonic to maintain the AVL invariant. That is, the mapped
+  -- morphism f needs to be (Ord a, Ord b) => a -> b, such that
+  --
+  --  >  x < x' => f x < f x'.
   fmap f (T r k) = T (fmapNode f r) k
 
 instance Foldable AVLTree where
-  foldMap f (T r _) = foldNode (\x y z -> x <> z <> y) mempty (fmapNode f r)
+  -- | The folding is in-order.
+  -- >>> let t = foldr insert empty [1..10]
+  -- >>> foldr (++) [] (pure <$> t)
+  -- >>> [1,2,3,4,5,6,7,8,9,10]
+  -- >>> foldr (+) 0 t
+  -- >>> 55
+foldMap f (T r _) = foldNode (\x y z -> x <> z <> y) mempty (fmapNode f r)
 
 instance Traversable AVLTree where
+  -- |Traversable instance for AVLNodes. This is an in-order traversal of the
+  -- subtree rooted at a given node.
+  -- >>> let t = foldr insert empty [1, 2, 3]
+  -- >>> traverse print t
+  -- >>> 1
+  -- >>> 2
+  -- >>> 3
   traverse f (T r k) = flip T k <$> traverseNode f r
 
-{- Context for a Zipper.
- - BC = Balanced Context,
- - xyC = xie Context, having gone through the y branch.
- -       For instance, LRC means a Leftie context, where one has taken the Right
- -       path in the subtree. -}
-
+-- |The context for an 'AVLTree'\'s 'Zipper'.
+-- The idea is that it represents an entire 'AVLTree', save for a hole in it.
+-- A 'Context' @n@ @a@ means an entire 'AVLTree' @a@, with a hole of height n.
+-- Its use is that, in a 'Zipper', we have a simple way to move around in the
+-- tree, starting at that hole.
+--
+-- See <http://strictlypositive.org/diff.pdf this> paper by Conor McBride for
+-- more information.
 data Context :: Nat -> * -> * where
+  -- A balanced context. BC isLeft x y means the traversal went left,
+  --  on a node Balanced x, where y is the subtree not taken in the
+  --  traversal.
   BC :: Bool -> a -> AVLNode n a -> Context (Succ n) a -> Context n a
+  -- A leftie context, where we've taken the right branch of the subtree.
   LRC :: a -> AVLNode (Succ n) a -> Context (Succ (Succ n)) a -> Context n a
+  -- A leftie context, where we've taken the left branch of the subtree.
   LLC :: a -> AVLNode n a -> Context (Succ (Succ n)) a -> Context (Succ n) a
+  -- A rightie context, where we've taken the left branch of the subtree.
   RLC :: a -> AVLNode (Succ n) a -> Context (Succ (Succ n)) a -> Context n a
+  -- A rightie context, where we've taken the right branch of the subtree.
   RRC :: a -> AVLNode n a -> Context (Succ (Succ n)) a -> Context (Succ n) a
+  -- The root context, where every traversal of an AVLTree starts.
   Root :: Integer -> Context n a
 deriving instance Show a => Show (Context n a)
 
+-- |A 'Zipper' is a useful construct for functional datastructure traversals.
+-- For us, it can be thought of as a pointer to a subtree in an 'AVLTree'.
+--
+-- See <http://yquem.inria.fr/~huet/PUBLIC/zip.pdf Functional Pearls: Zippers>
+-- for more information.
 data Zipper a = forall n. Zipper (AVLNode n a) (Context n a)
 deriving instance Show a => Show (Zipper a)
 
+-- |Gets the value at the root of the subtree pointed by that 'Zipper'.
 value :: Zipper a -> a
 value (Zipper (Balanced x _ _) _) = x
 value (Zipper (Leftie x _ _) _) = x
 value (Zipper (Rightie x _ _) _) = x
 value (Zipper Nil _) = error "Zipper points to Nil."
 
+-- |Constructs a 'Zipper' to the root of the given tree.
 unZip :: AVLTree a -> Zipper a
 unZip (T r k) = Zipper r (Root k)
 
+-- |Given a function that manipulates the tree size (number of nodes), and a
+-- 'Zipper', constructs an 'AVLTree' with the new height, by "zipping up" to
+-- the root of the tree pointed to by the 'Zipper'.
 zipUp :: (Integer -> Integer) -> Zipper a -> AVLTree a
 zipUp f (Zipper t (Root k)) = T t (f k)
 zipUp f z = zipUp f (up z)
 
+-- |Navigates up in a 'Zipper'.
 up :: Zipper a -> Zipper a
 up (Zipper x (LRC v y ctx)) = Zipper (Leftie v y x) ctx
 up (Zipper x (LLC v y ctx)) = Zipper (Leftie v x y) ctx
@@ -114,10 +144,12 @@ up (Zipper x (BC False v y ctx)) = Zipper (Balanced v y x) ctx
 -- TODO: Should I error out on Root?
 up z@(Zipper _ (Root _)) = z
 
+-- |Returns whether we can navigate up.
 canGoUp :: Zipper a -> Bool
 canGoUp (Zipper _ (Root _)) = False
 canGoUp _ = True
 
+-- |Navigates left in a 'Zipper'.
 left :: Zipper a -> Zipper a
 left (Zipper (Balanced x l r) ctx) = Zipper l (BC True x r ctx)
 left (Zipper (Leftie x l r) ctx) = Zipper l (LLC x r ctx)
@@ -125,6 +157,14 @@ left (Zipper (Rightie x l r) ctx) = Zipper l (RLC x r ctx)
 -- TODO: Should I error out on Nil?
 left z = z
 
+-- |Returns whether we can navigate left.
+canGoLeft :: Zipper a -> Bool
+canGoLeft (Zipper (Rightie _ Nil _) _) = False
+canGoLeft (Zipper (Balanced _ Nil _) _) = False
+canGoLeft (Zipper Nil _) = False
+canGoLeft _ = True
+
+-- |Navigates right in a 'Zipper'.
 right :: Zipper a -> Zipper a
 right (Zipper (Rightie x l r) ctx) = Zipper r (RRC x l ctx)
 right (Zipper (Leftie x l r) ctx) = Zipper r (LRC x l ctx)
@@ -132,18 +172,14 @@ right (Zipper (Balanced x l r) ctx) = Zipper r (BC False x l ctx)
 -- TODO: Should I error out on Nil?
 right z = z
 
-canGoLeft :: Zipper a -> Bool
-canGoLeft (Zipper (Rightie _ Nil _) _) = False
-canGoLeft (Zipper (Balanced _ Nil _) _) = False
-canGoLeft (Zipper Nil _) = False
-canGoLeft _ = True
-
+-- |Returns whether we can navigate right.
 canGoRight :: Zipper a -> Bool
 canGoRight (Zipper (Leftie _ _ Nil) _) = False
 canGoRight (Zipper (Balanced _ _ Nil) _ ) = False
 canGoRight (Zipper Nil _) = False
 canGoRight _ = True
 
+-- |Returns whether the pointed to subtree is a left child of its parent.
 isLeft :: Zipper a -> Bool
 isLeft z | not (canGoUp z) = False
 isLeft (Zipper _ (BC False _ _ _)) = False
@@ -151,12 +187,17 @@ isLeft (Zipper _ (RRC _ _ _)) = False
 isLeft (Zipper _ (LRC _ _ _ )) = False
 isLeft _ = True
 
+-- |Returns whether the pointed to subtree is a right child of its parent.
 isRight :: Zipper a -> Bool
 isRight = (&&) <$> canGoUp <*> (not . isLeft)
 
+-- |Returns whether the pointed to subtree is a leaf.
 isLeaf :: Zipper a -> Bool
 isLeaf = (&&) <$> (not . canGoLeft) <*> (not . canGoRight)
 
+-- |Descends (never ascends) to a subtree whose root has a given value.
+-- If no such subtree exists, points to a 'Nil' where the value would be found,
+--  were it to exist.
 zipTo :: Ord a => a -> Zipper a -> Zipper a
 zipTo _ z@(Zipper Nil _) = z
 zipTo x z = let v = value z
@@ -165,6 +206,11 @@ zipTo x z = let v = value z
                  LT -> zipTo x $ left z
                  GT -> zipTo x $ right z
 
+-- |Insert an 'AVLNode' of height (n + 1) in a 'Context' with a hole of size n.
+-- Since this cannot be done in the usual way, rotations are used to return
+-- an 'AVLTree' that may nothave the same height as the 'Context'\'s tree did,
+-- or have the same structure, but holds the same values, and has this enlarged
+--  'AVLNode' in it.
 insertUnbalancedAt :: AVLNode (Succ n) a -> Context n a -> AVLTree a
 insertUnbalancedAt t (LRC v y ctx) = zipUp (+1) $ Zipper (Balanced v y t) ctx
 insertUnbalancedAt t (RLC v y ctx) = zipUp (+1) $ Zipper (Balanced v t y) ctx
@@ -210,33 +256,47 @@ insertUnbalancedAt (Rightie b g p) (BC False a d ctx) = goUp
 insertUnbalancedAt t (BC False a d ctx) = insertUnbalancedAt (Rightie a d t) ctx
 insertUnbalancedAt t (BC True a d ctx) = insertUnbalancedAt (Leftie a t d) ctx
 
+-- |Given a 'Zipper' to a node in the tree, returns a 'Zipper' to the successor
+-- of this node. If no such successor exists, returns 'Nothing'.
 zipToSuccessor :: Zipper a -> Maybe (Zipper a)
 zipToSuccessor z | canGoRight z = Just . zipToSmallest $ right z
                  | otherwise = let parent = zipToFirstLeftChild z
                                in  up <$> parent
+-- |Given a 'Zipper' to a node in the tree, returns a Zipper to the predecessor
+-- of this node. If no such predecessor exists, returns 'Nothing'.
 zipToPredecessor :: Zipper a -> Maybe (Zipper a)
 zipToPredecessor z | canGoLeft z = Just . zipToSmallest $ left z
                    | otherwise = let parent = zipToFirstRightChild z
                                  in up <$> parent
-
+-- |Given a 'Zipper' to a node @X@ in the tree, returns a 'Zipper' to the
+-- smallest node in the subtree rooted at @X@.
 zipToSmallest :: Zipper a -> Zipper a
 zipToSmallest z | canGoLeft z = zipToSmallest (left z)
                 | otherwise = z
-
+-- |Given a 'Zipper' to a node @X@ in the tree, returns a 'Zipper' to the
+-- greatest node in the subtree rooted at @X@.
 zipToGreatest :: Zipper a -> Zipper a
 zipToGreatest z | canGoRight z = zipToGreatest (right z)
                 | otherwise = z
 
+-- |Given a 'Zipper' @Z@, which points to a subtree @S@, returns a 'Zipper' to
+-- the first ancestor of @S@ which is a left child of its parent. If such an
+-- ancestor does not exist, returns 'Nothing'.
 zipToFirstLeftChild :: Zipper a -> Maybe (Zipper a)
 zipToFirstLeftChild z | isLeft z = Just z
 zipToFirstLeftChild z | canGoUp z = zipToFirstLeftChild (up z)
                       | otherwise = Nothing
 
+-- |Given a 'Zipper' @Z@, which points to a subtree @S@, returns a 'Zipper' to
+-- the first ancestor of @S@ which is a right child of its parent. If such an
+-- ancestor does not exist, returns 'Nothing'.
 zipToFirstRightChild :: Zipper a -> Maybe (Zipper a)
 zipToFirstRightChild z | isRight z = Just z
 zipToFirstRightChild z | canGoUp z = zipToFirstRightChild (up z)
                        | otherwise = Nothing
 
+-- |Replaces a given value by another, in the 'AVLTree' represented by a
+-- 'Context'.
 fixContext :: forall a n. Eq a => a -> a -> Context n a -> Context n a
 fixContext k k' = go
   where
@@ -249,6 +309,9 @@ fixContext k k' = go
     go (RRC x y ctx) = RRC (z x) y (go ctx)
     go (RLC x y ctx) = RLC (z x) y (go ctx)
 
+-- |Given a 'Zipper' @Z@, deletes the value at the root of the subtree pointed
+-- to by @Z@. It returns a modified 'AVLTree' with this change applied.
+-- The removal is straight-up BST removal, folowed by an AVL rebalancing.
 deleteBST :: Eq a => Zipper a -> AVLTree a
 deleteBST (Zipper (Balanced _ Nil Nil) ctx) = rebalance Nil ctx
 deleteBST (Zipper (Rightie _ Nil r) ctx) = rebalance r ctx
@@ -273,6 +336,11 @@ deleteBST z@(Zipper (Balanced k _ _) _) =
        _ -> error "The impossible has happened, bad successor found."
 deleteBST (Zipper Nil _) = error "You cannot delete Nil."
 
+-- | Given an 'AVLNode' @n@ @a@, and a 'Context' with a hole of size @(n + 1)@,
+-- returns an 'AVLTree' @a@ with the 'AVLNode' being placed in that 'Context'.
+-- Since this cannot be done normally, it uses rotations to return an 'AVLTree'
+-- that has the same elements as the 'Context' and the 'AVLNode' together,
+-- but may have a different structure than the tree the 'Context' represented.
 rebalance :: forall a n. AVLNode n a -> Context (Succ n) a -> AVLTree a
 rebalance t (Root k) = (T t (k - 1))
 rebalance t (BC True a d ctx) = zipUp (subtract 1) $ Zipper (Rightie a t d) ctx

Data/Tree/AVL/Static.hs
@@ -15,28 +15,43 @@ module Data.Tree.AVL.Static (
 
 import Data.Tree.AVL.Static.Internal
 
+-- |An empty 'AVLTree'.
 empty :: AVLTree a
 empty = T Nil 0
 
+-- |The number of nodes of an 'AVLTree'.
 size :: AVLTree a -> Integer
 size (T _ k) = k
 
+-- |Insert a value into an 'AVLTree'.
+-- |If the value already exists, does nothing.
 insert :: Ord a => a -> AVLTree a -> AVLTree a
 insert x t = case zipTo x (unZip t) of
   Zipper Nil ctx -> insertUnbalancedAt (Balanced x Nil Nil) ctx
   _ -> t
 
+-- |Search for a node with a given value. Returns a 'Zipper' pointing to
+-- | a subtree whose root has that value, or 'Nothing' if the value is not
+-- | in the tree.
 search :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
 search x t = case zipTo x (unZip t) of
   Zipper Nil _ -> Nothing
   z -> Just z
 
+-- |Returns a 'Zipper' to the predecessor of a value in a tree.
+-- |Note the value needs to exist in the tree. If the value does not exist in
+-- | the tree, or it has no predecessor, returns 'Nothing'.
 predecessor :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
 predecessor x t = search x t >>= zipToPredecessor
 
+-- |Returns a 'Zipper' to the successor of a value in a tree.
+-- |Note the value needs to exist in the tree. If the value does not exist in
+-- | the tree, or it has no successor, returns 'Nothing'.
 successor :: Ord a => a -> AVLTree a -> Maybe (Zipper a)
 successor x t = search x t >>= zipToSuccessor
 
+-- |Deletes a value from an 'AVLTree'. If the value does not exist in the tree,
+-- | does nothing.
 delete :: Ord a => a -> AVLTree a -> AVLTree a
 delete x t = case zipTo x (unZip t) of
   Zipper Nil _ -> t

commit b8ff39644e31779932dd9f3d55ddd30bd2f7df6f

Fixing Foldable instance.
Commit 1895f880 introduced a bug due to removing the whitespace before
foldMap.

Data/Tree/AVL/Static/Internal.hs
@@ -70,7 +70,7 @@ instance Foldable AVLTree where
   -- >>> [1,2,3,4,5,6,7,8,9,10]
   -- >>> foldr (+) 0 t
   -- >>> 55
-foldMap f (T r _) = foldNode (\x y z -> x <> z <> y) mempty (fmapNode f r)
+  foldMap f (T r _) = foldNode (\x y z -> x <> z <> y) mempty (fmapNode f r)
 
 instance Traversable AVLTree where
   -- |Traversable instance for AVLNodes. This is an in-order traversal of the